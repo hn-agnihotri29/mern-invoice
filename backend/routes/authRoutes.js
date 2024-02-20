6733cac302f4e3e0c8e1af4e55c685291b20a67f
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2024-02-20 13:32:31 +0530

    feat: add forgot and reset password functionality

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -5,6 +5,7 @@ import loginUser from "../controllers/auth/loginController.js";
 import { loginLimiter } from "../middleware/apiLimiter.js";
 import newAccessToken from "../controllers/auth/refreshTokenController.js";
 import resendEmailVerificationToken from "../controllers/auth/resendVerifyEmailController.js";
+import { resetPassword, resetPasswordRequest  } from "../controllers/auth/passwordResetController.js"; 
 
 const router = express.Router();
 
@@ -17,5 +18,9 @@ router.post("/login", loginLimiter, loginUser);
 router.get("/new_access_token", newAccessToken);
 
 router.post("/resend_email_token", resendEmailVerificationToken);
+
+router.post("/reset_password_request", resetPasswordRequest);
+
+router.post("/reset_password", resetPassword);
 
 export default router
