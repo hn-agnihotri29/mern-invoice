243dda7ecc2a6ebed3be383a603b1f4c94eff4d2
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2024-02-27 13:31:15 +0530

    feat: add delete my user account

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import getUserProfile from "../controllers/user/getUserProfile.js";
 import checkAuth from "../middleware/checkAuthMiddleware.js";
 import updateUserProfile from "../controllers/user/updateUserProfile.js";
+import deleteMyAccount from "../controllers/user/deleteMyAccount.js";
 
 const router = express.Router();
 
@@ -9,6 +10,7 @@ router
 	.route("/profile")
 	.get(checkAuth, getUserProfile)
 	.patch(checkAuth, updateUserProfile)
+	.delete(checkAuth, deleteMyAccount)
 
 
 
