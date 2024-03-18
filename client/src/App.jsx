bb930c4f5fb9492466452f621559def890142088
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2024-03-18 20:27:14 +0530

    feat: add home page and styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,34 @@
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { Route, Routes } from "react-router-dom";
+import { customTheme } from "./customTheme";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+import Footer from "./components/Footer";
+import Layout from "./components/Layout";
+import NotFound from "./components/NotFound";
+import useTitle from "./hooks/useTitle";
+import HomePage from "./pages/HomePage";
+
 
 const App = () => {
+  useTitle("MERN Invoice - Home");
+
   return (
-    <div>
-      <h1>Welcome to  the Invoice!!!</h1>
-    </div>
+    <ThemeProvider theme={customTheme}>
+      <CssBaseline />
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<HomePage />} />
+
+
+
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+        <Footer />
+			  <ToastContainer theme="dark" />
+    </ThemeProvider>
   )
 }
 
